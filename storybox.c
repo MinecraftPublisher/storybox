62b44496f127a2d499722253673ecf8dbdfb783d
martia_f <44898501+MinecraftPublisher@users.noreply.github.com>
2023-11-23 15:38:32 +0330

    Added choice command: Move with `space` and choose with `enter`

--- storybox.c
+++ storybox.c
@@ -2,5 +2,10 @@
 
 int main() {
     __init_storybox_tools();
-    typewriter("Hello!\nHow are you doing?");
+
+    typewriter("-- STORYBOX --\nA text-based game development toolkit.");
+    string arr[] = { "Option 1", "Option 2", "Option 3" };
+    printf("YOU CHOSE: {%s}\n", arr[choose(3, arr)]);
+
+    return 0;
 }
